124120ef074f953e62ce08d3d2120a7e921c05f0
Luciano Xu <LucianoXu@foxmail.com>
2020-04-19 16:58:19 +0800

    2020/4/19 添加矩阵输出，矩阵直积，矩阵乘法，以及厄米矩阵相关的优化

--- mkl_append/mkl_append.cpp
+++ mkl_append/mkl_append.cpp
@@ -1,4 +1,5 @@
-#include "mkl.h"
+#include "mkl_append.h"
+using namespace std;
 
 inline lapack_complex_double conj(lapack_complex_double a) {
 	lapack_complex_double r;
@@ -30,7 +31,23 @@ inline lapack_complex_double operator*(lapack_complex_double a, lapack_complex_d
 	return r;
 }
 
-void zpma(const lapack_complex_double* H1, const lapack_complex_double* H2, int n, lapack_complex_double* Hr) {
+inline ostream & operator <<(ostream & os, lapack_complex_double z) {
+	os << "(" << z.real << "." << z.imag << ")";
+	return os;
+}
+
+void zgem_out(lapack_complex_double* G, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			std::cout << G[i * n + j];
+			std::cout << "\t";
+		}
+		std::cout << std::endl;
+	}
+}
+
+
+void zhpma(const lapack_complex_double* H1, const lapack_complex_double* H2, int n, lapack_complex_double* Hr) {
 	int N = n * (n + 1) / 2;
 	for (int i = 0; i < N; i++)
 	{
@@ -39,7 +56,7 @@ void zpma(const lapack_complex_double* H1, const lapack_complex_double* H2, int 
 }
 //Compute Hr(mn*mn):=H1(m*m)(DirectProduct)H2(n*n)
 //packed storage, L, row major, a[j+i*(i+1)/2]->A[i][j]
-void zplmk(const lapack_complex_double* H1, int m, const lapack_complex_double* H2, int n, lapack_complex_double* Hr)
+void zhplrmk(const lapack_complex_double* H1, int m, const lapack_complex_double* H2, int n, lapack_complex_double* Hr)
 {
 	for (int i1 = 0; i1 < m; i1++)
 	{
@@ -72,6 +89,52 @@ void zplmk(const lapack_complex_double* H1, int m, const lapack_complex_double* 
 				{
 					int kj = j1 * n + j2;
 					Hr[n_ahead_Hr + kj] = H1[n_ahead_H1 + j1] * conj(H2[j2 * (j2 + 1) / 2 + i2]);
+				}
+			}
+		}
+	}
+}
+
+void zgemk(const lapack_complex_double* G1, int m, int n, lapack_complex_double* G2, int p, int q, lapack_complex_double* R) {
+	for (int i1 = 0; i1 < m; i1++) {
+		for (int i2 = 0; i2 < p; i2++) {
+			int _index = (i1 * p + i2)*n*q;
+			for (int j1 = 0; j1 < n; j1++) {
+				lapack_complex_double z = G1[i1 * n + j1];
+				int _index2 = j1 * q;
+				for (int j2 = 0; j2 < q; j2++) {
+					R[_index + _index2 + j2] = z * G2[i2 * q + j2];
+				}
+			}
+		}
+	}
+}
+
+//Compute Hr(mn*mn):=H1(m*m)(DirectProduct)H2(n*n)
+//full storage, row major,
+//H1 should at least be stored in lower triangle, row major, H2 should be full
+void zhelrmk(const lapack_complex_double* H1, int m, const lapack_complex_double* H2, int n, lapack_complex_double* Hr) {
+	for (int i1 = 0; i1 < m; i1++) {
+		//先对角线
+		lapack_complex_double z = H1[i1 * m + i1];
+		for (int i2 = 0; i2 < n; i2++) {
+			int _index = (i1*n + i2) * m * n + i1*n;
+			for (int j2 = 0; j2 < n ; j2++) {
+				Hr[_index + j2] = z * H2[i2 * n + j2];
+			}
+		}
+		//再下三角
+		for (int j1 = 0; j1 < m; j1++) {
+			lapack_complex_double z1 = H1[i1 * m + j1];
+			lapack_complex_double z2 = conj(z1);
+			for (int i2 = 0; i2 < n; i2++) {
+				int _index1 = (i1 * n + i2) * m * n + j1 * n;
+				int _index2 = (j1 * n + i2) * m * n + i1 * n;
+				for (int j2 = 0; j2 < n; j2++) {
+					//处理其中一半
+					Hr[_index1 + j2] = z1 * H2[i2 * n + j2];
+					//处理共轭的另一半
+					Hr[_index2 + j2] = z2 * H2[i2 * n + j2];
 				}
 			}
 		}
--- mkl_append/mkl_append.h
+++ mkl_append/mkl_append.h
@@ -1,15 +1,31 @@
 #ifndef __MKL_APPEND_H__
 #define __MKL_APPEND_H__
 #include <mkl.h>
+#include <iostream>
 
 inline lapack_complex_double conj(lapack_complex_double a);
 inline lapack_complex_double operator+(lapack_complex_double a, lapack_complex_double b);
 inline lapack_complex_double operator-(lapack_complex_double a, lapack_complex_double b);
 inline lapack_complex_double operator*(lapack_complex_double a, lapack_complex_double b);
 
-void zpma(const lapack_complex_double* H1, const lapack_complex_double* H2, int n, lapack_complex_double* Hr);
+inline std::ostream & operator <<(std::ostream& os, lapack_complex_double z);
 
-void zplmk(const lapack_complex_double* H1, int m, const lapack_complex_double* H2, int n, lapack_complex_double* Hr);
+//output the matrix(vector) G(n*m) with ostream
+void zgem_out(lapack_complex_double* G, int m, int n);
 
+//Hermitan matrix, packed storage, add
+void zhpma(const lapack_complex_double* H1, const lapack_complex_double* H2, int n, lapack_complex_double* Hr);
+
+//Compute Hr(mn*mn):=H1(m*m)(DirectProduct)H2(n*n)
+//packed storage, L, row major, a[j+i*(i+1)/2]->A[i][j]
+void zhplrmk(const lapack_complex_double* H1, int m, const lapack_complex_double* H2, int n, lapack_complex_double* Hr);
+
+//DirectProduct for general matrixs(vectors)
+void zgemk(const lapack_complex_double* G1, int m, int n, lapack_complex_double* G2, int p, int q, lapack_complex_double* R);
+
+//Compute Hr(mn*mn):=H1(m*m)(DirectProduct)H2(n*n)
+//full storage, L, row major,
+//H1,H2 should be stored in lower triangle, row major
+void zhelrmk(const lapack_complex_double* H1, int m, const lapack_complex_double* H2, int n, lapack_complex_double* Hr);
 
 #endif
